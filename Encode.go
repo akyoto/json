c0916d4a6a54def8116adbf73b0f60de55b8d483
Eduard Urbach <e.urbach@gmail.com>
2019-09-04 09:40:46 +0900

    Added basic encoder

---
+++ Encode.go
@@ -0,0 +1,77 @@
+package json
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+)
+
+var (
+	bracketStart        = []byte{'{'}
+	bracketEnd          = []byte{'}'}
+	stringIndicator     = []byte{'"'}
+	fieldValueSeparator = []byte{'"', ':'}
+	comma               = []byte{','}
+)
+
+// Encode encodes the object to JSON
+// and writes the output to the given writer.
+func Encode(object interface{}, writer io.Writer) error {
+	return writeValue(writer, reflect.ValueOf(object))
+}
+
+// writeValue writes the given value in JSON-encoded form to the writer.
+// nolint:errcheck
+func writeValue(writer io.Writer, v reflect.Value) error {
+	stringWriter := writer.(io.StringWriter)
+	t := v.Type()
+
+	if v.Kind() == reflect.Ptr {
+		t = t.Elem()
+		v = v.Elem()
+	}
+
+	switch v.Kind() {
+	case reflect.String:
+		writer.Write(stringIndicator)
+		stringWriter.WriteString(v.String())
+		writer.Write(stringIndicator)
+
+	case reflect.Int:
+		// TODO: Avoid allocation
+		x := strconv.FormatInt(v.Int(), 10)
+		stringWriter.WriteString(x)
+
+	case reflect.Struct:
+		writer.Write(bracketStart)
+		fieldCount := t.NumField()
+
+		for i := 0; i < fieldCount; i++ {
+			// Key
+			tField := t.Field(i)
+			writer.Write(stringIndicator)
+			stringWriter.WriteString(tField.Name)
+			writer.Write(fieldValueSeparator)
+
+			// Value
+			vField := v.Field(i)
+			err := writeValue(writer, vField)
+
+			if err != nil {
+				return err
+			}
+
+			if i != fieldCount-1 {
+				writer.Write(comma)
+			}
+		}
+
+		writer.Write(bracketEnd)
+
+	default:
+		return fmt.Errorf("Can't encode field of kind '%v'", v.Kind())
+	}
+
+	return nil
+}
---
+++ Encode_test.go
@@ -0,0 +1,62 @@
+package json_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	stdJSON "encoding/json"
+
+	"github.com/akyoto/json"
+	jsoniter "github.com/json-iterator/go"
+)
+
+var object = &Movie{
+	Title:    "The Last Samurai",
+	Director: "Edward Zwick",
+}
+
+func TestEncode(t *testing.T) {
+	err := json.Encode(object, ioutil.Discard)
+
+	if err != nil {
+		t.Fatalf("Encode failed: %v", err)
+	}
+}
+
+func BenchmarkEncode(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		err := json.Encode(object, ioutil.Discard)
+
+		if err != nil {
+			b.Fatalf("Encode failed: %v", err)
+		}
+	}
+}
+
+func BenchmarkEncodeStd(b *testing.B) {
+	b.ReportAllocs()
+	encoder := stdJSON.NewEncoder(ioutil.Discard)
+
+	for i := 0; i < b.N; i++ {
+		err := encoder.Encode(object)
+
+		if err != nil {
+			b.Fatalf("Encode failed: %v", err)
+		}
+	}
+}
+
+func BenchmarkEncodeJsoniter(b *testing.B) {
+	b.ReportAllocs()
+	encoder := jsoniter.NewEncoder(ioutil.Discard)
+
+	for i := 0; i < b.N; i++ {
+		err := encoder.Encode(object)
+
+		if err != nil {
+			b.Fatalf("Encode failed: %v", err)
+		}
+	}
+}
